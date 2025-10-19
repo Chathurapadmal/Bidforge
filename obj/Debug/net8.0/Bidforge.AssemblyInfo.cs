e35306ec5e4bf4774e33f97c4d62d74af0b75d0b
chathura <chathura.p.ranawaka@gmail.com>
2025-10-20 02:39:00 +0530

    resolved merge conflict

--- obj/Debug/net8.0/Bidforge.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bidforge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bidforge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1117cd1fba4f6a2af43112aea745e2b2c8264415")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bidforge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bidforge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
